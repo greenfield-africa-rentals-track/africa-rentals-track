c7e71a09284f1fed712268f62864eaed114763c6
Abidijihed <abidij55@gmail.com>
2021-04-01 00:32:06 +0200

    jihed add cards

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { InformationComponent } from './commponents/information/information.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    InformationComponent
+    InformationComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
