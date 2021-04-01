0a09527b246ebe7b16360e23075fbe6f037fe5b1
Dali-Hili <you@example.com>
2021-04-01 12:44:04 +0200

    setted search-input

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InformationComponent } from './commponents/information/information.component';
+//dali
 import { NavbarComponent } from './commponents/navbar/navbar.component';
 import {MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -25,6 +26,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
     MatIconModule,
     MatInputModule,
     BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
