45822649a5bdd16f2e7aafc1fe67181725116b1f
dhiaaouichaoui <dhia12aouichaoui@gmail.com>
2021-04-02 10:27:36 +0100

    controller

--- client/src/app/commponents/product/product.component.ts
+++ client/src/app/commponents/product/product.component.ts
@@ -9,6 +9,7 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   options: string[] = ['Apple', 'Orange', 'Banana']
+  img: string[]= ['https://www.unitedrentals.com/sites/default/files/styles/square_1_1_414x414/public/category/300/300-0750_0.jpg']
 
   ngOnInit(): void {
   }
