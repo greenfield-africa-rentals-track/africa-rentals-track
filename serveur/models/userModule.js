e1dfc9f4025deee097cb965b0972ecee8de91aae
Dali-Hili <you@example.com>
2021-04-02 23:07:27 +0200

    authentication works

--- serveur/models/userModule.js
+++ serveur/models/userModule.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
@@ -7,5 +7,22 @@ const userSchema = new mongoose.Schema({
     password: String,
     phoneNumber: String
 });
+userSchema.pre('save', async function (next){
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hachedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hachedPassword;
+        next();
+    }catch(err){
+        next(err);
+    }
+})
 
 module.exports = mongoose.model("user", userSchema)
+
+
+
+
+
+
+
