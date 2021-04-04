fcedaed68bda3f246485cd0c5433d19279d389c4
NadhemBacha <nadhembecha@gmail.com>
2021-04-04 16:20:13 +0100

    changes for carousel done

--- client/src/app/commponents/card-product/card-product.component.ts
+++ client/src/app/commponents/card-product/card-product.component.ts
@@ -10,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardProductComponent implements OnInit {
  
- 
+ dataList=[
+   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
+   {name: 'Boom Lifts', image:'https://www.genielift.com/images/default-source/product-images/telescopic-booms/s-40-s-45/s-45_cutout-1.jpg?sfvrsn=9b29291f_15' },
+   {name: 'Warehouse Forklifts', image:'https://miamitoolrental.com/wp-content/uploads/2017/11/8FGU25-700x700.jpg' },
+   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
+   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
+   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
+   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
+   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
+ ];
   constructor() { }
 
   ngOnInit(): void {
