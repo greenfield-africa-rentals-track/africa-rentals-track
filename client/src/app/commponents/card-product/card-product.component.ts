80813114d26192a4df0debe4caf9dd40a60e3ab8
dhiaaouichaoui <dhia12aouichaoui@gmail.com>
2021-04-06 14:07:09 +0100

    get product card

--- client/src/app/commponents/card-product/card-product.component.ts
+++ client/src/app/commponents/card-product/card-product.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
-
-
-
+import {AddToCartService} from '../../servise/add-to-cart.service'
 @Component({
   selector: 'app-card-product',
   templateUrl: './card-product.component.html',
@@ -10,22 +8,27 @@ import { Router, RouterModule, Routes } from '@angular/router';
 })
 export class CardProductComponent implements OnInit {
  
- dataList=[
-   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
-   {name: 'Boom Lifts', image:'https://www.genielift.com/images/default-source/product-images/telescopic-booms/s-40-s-45/s-45_cutout-1.jpg?sfvrsn=9b29291f_15' },
-   {name: 'Warehouse Forklifts', image:'https://miamitoolrental.com/wp-content/uploads/2017/11/8FGU25-700x700.jpg' },
-   {name: 'Scissor Lift', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
-   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
-   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
-   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
-   {name: 'Scissor Lifts', image:'https://cdn.shopify.com/s/files/1/0085/2593/8767/products/26322_1024x1024@2x.jpg?v=1610605728' },
- ];
-  constructor( private router:Router ) { }
+ dataList:any=[];
+
+  constructor( private router:Router,private cartService:AddToCartService) { }
+
+  
   functionON() {
     this.router.navigate(["/info"])
     
   }
+  getcardprod(){
+    
+    this.cartService.getcardProduct().subscribe((data)=>{
+      this.dataList = data
+      console.log(data,"hello")
+      
+     
+    })
+  }
+ 
   ngOnInit(): void {
+    this.getcardprod()
   }
 
 }
