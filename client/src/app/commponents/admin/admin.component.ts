a51c99fe23d789deebd0e75aa52a37d2cb286280
dhiaaouichaoui <dhia12aouichaoui@gmail.com>
2021-04-06 10:11:29 +0100

    mergin

--- client/src/app/commponents/admin/admin.component.ts
+++ client/src/app/commponents/admin/admin.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {ProductsService} from "../../services/products.service"
+import {ProductsService} from "../../services/products.service";
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+
+ 
+  
   productForm: FormGroup
   constructor(private add: FormBuilder,private objectService:ProductsService) { 
+    
+
     this.productForm = this.add.group({
       name: [''],
       imageurl: [''],
@@ -19,6 +25,7 @@ export class AdminComponent implements OnInit {
 
     });
   }
+  
   submit() {
       this.objectService
       .addProduct(this.productForm.value)
